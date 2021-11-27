e06baf7fe99de665367647f4328063fb263f4175
lepzulnag <gindre.matthieu@gmail.com>
2021-11-28 00:58:46 +0100

    🐛 Fix grubber call

--- library/index.js
+++ library/index.js
@@ -23,7 +23,7 @@ export async function compile() {
 
 export function patch(aliases) {
 	const directories = getOutputDirectories()
-	patchJsImports.apply(null, directories, aliases)
+	patchJsImports(directories, aliases)
 }
 
 /**
